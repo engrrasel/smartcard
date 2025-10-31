1f7218a5a92e43677bea88695fc637163fbea3d9
Rasel Mia <eng.raselmiah@gmail.com>
2025-10-31 19:51:51 +0600

    multiple crad creatio system added

--- app_accounts/models.py
+++ app_accounts/models.py
@@ -72,8 +72,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 # 🟨 User Profile Model
 class UserProfile(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    username = models.SlugField(max_length=100, unique=True, blank=True, null=True)
     full_name = models.CharField(max_length=150, blank=True, null=True)
+    username = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    
     job_title = models.CharField(max_length=100, blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     company_name = models.CharField(max_length=100, blank=True, null=True)
